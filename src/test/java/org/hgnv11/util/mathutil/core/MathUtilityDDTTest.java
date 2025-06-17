5c6d344cafdef2d633ffdebeef5482133a6e8d46
Hgnv11 <Hoangnvse172524@fpt.edu.vn>
2025-06-17 13:38:12 +0700

    Add 2 types of unit tests for MathUtility: TDD and DDT(T) using TestNG

--- src/test/java/org/hgnv11/util/mathutil/core/MathUtilityDDTTest.java
+++ src/test/java/org/hgnv11/util/mathutil/core/MathUtilityDDTTest.java
@@ -9,7 +9,7 @@ public class MathUtilityDDTTest {
 
     @DataProvider(name="evenDigitData")
     public Object[][] provideEvenDigitData(){
-        return new Object [][]{{123,2},{-227,4},{-432,6}, {-627,8}, {0,1}, {13,0}};
+        return new Object [][]{{123,2},{-227,4},{-432,6}, {-627,8}, {0,0}, {13,0}};
     }
 
     @Test(dataProvider = "evenDigitData")
