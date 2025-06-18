d57e241f41c2b72f30741d12b3ea212b6b4e1c5c
CaoThien <caothien0502@gmail.com>
2025-06-18 10:39:32 +0700

    update case pass

--- src/test/java/org/hgnv11/util/mathutil/core/MathUtilityDDTTest.java
+++ src/test/java/org/hgnv11/util/mathutil/core/MathUtilityDDTTest.java
@@ -6,29 +6,29 @@ import static org.testng.Assert.*;
 
 public class MathUtilityDDTTest {
 
-//    @DataProvider(name = "evenDigitData")
-//    public Object[][] provideEvenDigitData() {
-//        return new Object[][]{
-//                {123, 2},
-//                {-227, 4},
-//                {-432, 6},
-//                {627, 8},
-//                {0, 0},
-//                {13579, 0}
-//        };
-//    }
-
     @DataProvider(name = "evenDigitData")
     public Object[][] provideEvenDigitData() {
         return new Object[][]{
-                {123, 7},
+                {123, 2},
                 {-227, 4},
-                {-432, 5},
+                {-432, 6},
                 {627, 8},
                 {0, 0},
                 {13579, 0}
         };
     }
+
+//    @DataProvider(name = "evenDigitData")
+//    public Object[][] provideEvenDigitData() {
+//        return new Object[][]{
+//                {123, 7},
+//                {-227, 4},
+//                {-432, 5},
+//                {627, 8},
+//                {0, 0},
+//                {13579, 0}
+//        };
+//    }
 
     @Test(groups = {"sum"}, dataProvider = "evenDigitData")
     public void testSumEvenDigits_ValidCases(int input, int expected) {
